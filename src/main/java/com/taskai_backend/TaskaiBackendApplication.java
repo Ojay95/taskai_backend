b88a0b4cb01939194c4d87df8a236cd8303e0657
unknown <iamojay89@gmail.com>
2026-03-12 11:01:25 +0100

    first commit

---
+++ src/main/java/com/taskai_backend/TaskaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.taskai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskaiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskaiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taskai_backend/TaskaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskaiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
